2a60c00849a25ac3565f8561f76436f2528836cb
Carl Jørund Svensen <carl.svensen@cygni.se>
2023-05-26 12:38:20 +0200

    Cleaned up some code.

--- contract-server-impl/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into person (id, firstname, lastname) values (1, 'Ola', 'Normann');
-insert into person (id, firstname, lastname) values (2, '2', '2');
-insert into person (id, firstname, lastname) values (3, '3', '3');
